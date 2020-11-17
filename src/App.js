31cc5e227e586df68a2324d7e51be8870f28c04f
Alisha <alishahusain20@gmail.com>
2020-11-16 18:19:34 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Body Mass Index (BMI) Calculator Tool</h1>
+      <form>
+      <fieldset>
+         <label>
+           <p>Weight</p>
+           <input name="weight" />
+         </label>
+         <select name="w_u">
+         <option value="lbs">lbs</option>
+         <option value="kg">kg</option>
+       </select>
+       </fieldset>
+       <fieldset>
+         <label>
+           <p>Height (feet/inches)</p>
+         </label>
+
+         <select name="h_in">
+         <option value="4">4'</option>
+         <option value="5">5'</option>
+         <option value="6">6'</option>
+         <option value="7">7'</option>
+       </select>
+
+         <select name="h_in">
+         <option value="0">0"</option>
+         <option value="1">1"</option>
+         <option value="2">2"</option>
+         <option value="3">3"</option>
+         <option value="4">4"</option>
+         <option value="5">5"</option>
+         <option value="6">6"</option>
+         <option value="7">7"</option>
+         <option value="8">8"</option>
+         <option value="9">9"</option>
+         <option value="10">10"</option>
+         <option value="11">11"</option>
+       </select>
+
+       </fieldset>
+       <label>
+           <p>OR</p>
+         </label>
+       <fieldset>
+         <label>
+           <p>Height (cm/inches) </p>
+           <input name="height" />
+         </label>
+         <select name="h_u">
+         <option value="cm">cm</option>
+         <option value="in">inches</option>
+       </select>
+       </fieldset>
+
+
+       <button type="submit" onClick="" >Submit</button>
+      </form>
     </div>
   );
 }
